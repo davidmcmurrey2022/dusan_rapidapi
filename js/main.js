0d149c07336d91375a535982174fd94bb97751ae
David McMurrey <95498133+davidmcmurrey2022@users.noreply.github.com>
2023-05-24 16:53:09 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+import { MY_API_KEY } from './config.js';
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/language_code=en", {
 	"method": "GET",
 	"headers": {
 		 "X-RapidAPI-Host":  "quotes15.p.rapidapi.com",
-		 "X-RapidAPI-Key":  "9de2263e54msh57383d13043c0d4p1c23aejsn4bb1855cfb27"
+		 "X-RapidAPI-Key":  MY_API_KEY
 	}
 })
 .then(response => response.json())
 .then(response => {
 	console.log(response);
-	console.log(response.content);
+
+	document.getElementById('quote').innerHTML = response.content;
+	document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
 })
 .catch(err => {
-	console.error(err);
+	console.log(err);
 });
